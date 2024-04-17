c0c1f89c3a6aa3d8bc2b4419c47fe0f6c9aef2d2
Artem.T <poquepoque27@gmail.com>
2024-04-17 16:32:03 +0500

    base path

--- caravan-game-client/vite.config.ts
+++ caravan-game-client/vite.config.ts
@@ -7,7 +7,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
-  base: '',
+  base: '/',
   server: {
     port: 8080,
     // host: '0.0.0.0',
